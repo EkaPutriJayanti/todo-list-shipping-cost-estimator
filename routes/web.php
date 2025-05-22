391905c89fe653ae68ce6a7cc4f85b9e8239af10
EkaPutriJayanti <eka.putri.jayanti@student.undiksha.ac.id>
2025-05-22 10:39:36 +0800

    chore: update testing env and routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\CalculatorController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\LoanCalculatorController;
+use App\Http\Controllers\ShippingController;
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,3 +25,7 @@ Route::post('/calculator', [CalculatorController::class, 'calculate']);
 
 Route::get('/loan', [LoanCalculatorController::class, 'index']);
 Route::post('/loan', [LoanCalculatorController::class, 'calculate']);
+
+Route::get('/shipping', [ShippingController::class, 'index']);
+Route::post('/shipping/calculate', [ShippingController::class, 'calculate'])->name('shipping.calculate');
+
--- tests/Feature/FeatureTodoTest.php
+++ tests/Feature/FeatureTodoTest.php
@@ -5,35 +5,51 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class FeatureTodoTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function testLoadHomePage(): void
+    public function testStoreDataActivity()
     {
+        // 1. cek url yang diakses
         $response = $this->get(route('dashboard'));
-
         $response->assertStatus(200);
+        $response->assertSee('Enter an activity');
+        
+        // 2. user mengirim data ke server
+        $data = [
+            'name' => 'Testing',
+        ];
+        $storedata = $this->post(route('item.store'), $data);
+        $storedata = $this->post(route('item.store'), $data);
+
+        // 3. apakah data berhasil ditambahkan ke database
+        $storedata->assertStatus(Response::HTTP_FOUND);
+        $this->assertDatabaseHas('tasks', [
+            'name' => 'Testing',
+        ]);
+
+        // 4. Redirect ke dashboard
+        $storedata->assertRedirect(route('dashboard'));
     }
 
-
-    public function testCreateActivity(): void
+    public function testDeleteDataActivity()
     {
+        // 1. cek url yang diakses
         $response = $this->get(route('dashboard'));
+        $response->assertStatus(200);
+        $response->assertSee('Enter an activity');
 
-        $data = [
-            'item' => 'Belajar Testing',
-        ];
+        // 2. user mengirim data ke server
+        $storedata = $this->delete(route('item.destroy', ['id' => 3]));
 
-        $response = $this->post(route('item.store'), $data);
-
-        $response->assertStatus(302);
-        $response->assertRedirect(route('dashboard'));
-
-        $this->assertDatabaseHas('tasks', [
-            'name' => 'Belajar Testing',
+        // 3. Apakah data berhasil dihapus dari database
+        $storedata->assertStatus(Response::HTTP_FOUND);
+        $this->assertDatabaseMissing('tasks', [
+            'id' => 3,
         ]);
+
+        // 4. Redirect ke dashboard
+        $storedata->assertRedirect(route('dashboard'));
     }
 }
